4f0f35a11a941c57100ae7fa04fe331be15f0368
AndrewKoldyrev <raven4791f@mail.ru>
2023-06-21 13:13:57 +0700

    [homework-9] - added redux (reducers and actions)

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Navigation } from './Navigation/Navigation/NAvigation'
 import { TokenProvider } from './Infrastructure/TokenProvider/TokenProvider'
+import { Provider } from 'react-redux'
+import store from './redux/store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <TokenProvider>
-    <Navigation />
-  </TokenProvider>
+  <Provider store={store}>
+    <TokenProvider>
+      <Navigation />
+    </TokenProvider>
+  </Provider>
 )
---
+++ src/redux/reducers/costItemReducer.ts
@@ -0,0 +1,50 @@
+
+import { type AddCostItemAction, type DeleteCostItemAction } from '../actions/costItemActions'
+import { CATEGORIES_ADD, CATEGORIES_DELETE } from '../actions/actionTypes'
+import { type CostItemType } from '../../CostList/Types/CostItemType'
+
+const initialState: CostItemType[] = [
+  {
+    name: 'Яблоко',
+    cost: 100,
+    category: 'Фрукты',
+    store: 'Пятёрочка'
+  },
+  {
+    name: 'Ванна',
+    cost: 100000,
+    category: 'Сантехника',
+    store: 'Водолей'
+  },
+  {
+    name: 'Монитор',
+    cost: 10000,
+    category: 'Техника',
+    store: 'DNS'
+  },
+  {
+    name: 'Гарри поттер и Узник Азкабана',
+    cost: 1000,
+    category: 'Книга',
+    store: 'Книжный мир'
+  }
+]
+
+type costItemAction = AddCostItemAction | DeleteCostItemAction
+
+export default function costItemReducer (state: CostItemType[] = initialState, action: costItemAction): CostItemType[] {
+  switch (action.type) {
+    case CATEGORIES_ADD: {
+      const newState: CostItemType[] = state.concat()
+      newState.push((action as AddCostItemAction).costItem)
+      return newState
+    }
+    case CATEGORIES_DELETE: {
+      const newState: CostItemType[] = state.concat()
+      newState.splice((action as DeleteCostItemAction).id, 1)
+      return newState
+    }
+    default:
+      return state
+  }
+}

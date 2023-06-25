b1224f82acf4b354207121f59a877bc04f1498e9
AndrewKoldyrev <raven4791f@mail.ru>
2023-06-25 21:25:12 +0700

    homework-9-thunk-examples

--- src/redux/actions/actionTypes.ts
+++ src/redux/actions/actionTypes.ts
@@ -3,3 +3,6 @@ export const COSTITEM_DELETE: string = 'COSTITEM_DELETE'
 
 export const CATEGORIES_ADD: string = 'CATEGORIES_ADD'
 export const CATEGORIES_DELETE: string = 'CATEGORIES_DELETE'
+
+export const STORE_SAVE_STORE: string = 'STORE_SAVE_STORE'
+export const STORE_GET_STORE: string = 'STORE_GET_STORE'
--- src/redux/reducers/rootReducer.ts
+++ src/redux/reducers/rootReducer.ts
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
 import categoryReducer from './categoryReducer'
 import costItemReducer from './costItemReducer'
+import storeReducer from './storeReducer'
 
 export default combineReducers({
-  categoryReducer, costItemReducer
+  categoryReducer, costItemReducer, storeReducer
 })
